d52bfaf9d7a3275436d8c55c35d303ab1aaa8c0c
unknown <Leonardo>
2025-04-07 23:25:39 -0300

    Adicionando e Aplicando o lombok para redução de codigo

--- src/main/java/dev/lest/CadastroNinja/Missoes/MissoesNinjaModel.java
+++ src/main/java/dev/lest/CadastroNinja/Missoes/MissoesNinjaModel.java
@@ -3,11 +3,17 @@ package dev.lest.CadastroNinja.Missoes;
 
 import dev.lest.CadastroNinja.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesNinjaModel {
 
     @Id
@@ -18,36 +24,4 @@ public class MissoesNinjaModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-
-    public MissoesNinjaModel() {}
-
-    public MissoesNinjaModel(long id, String nomeMissao, String dificuldadeMissao) {
-        this.id = id;
-        this.nome = nomeMissao;
-        this.dificuldade = dificuldadeMissao;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 }
--- src/main/java/dev/lest/CadastroNinja/Ninjas/NinjaModel.java
+++ src/main/java/dev/lest/CadastroNinja/Ninjas/NinjaModel.java
@@ -2,13 +2,13 @@ package dev.lest.CadastroNinja.Ninjas;
 
 import dev.lest.CadastroNinja.Missoes.MissoesNinjaModel;
 import jakarta.persistence.*;
+import lombok.*;
 
-import java.util.List;
-
-
-// Transforma uma classe em Entidade do DB
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -21,36 +21,6 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Fk da missao
     private MissoesNinjaModel missoes;
 
-    public NinjaModel() {}
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
+// Transforma uma classe em Entidade do DB
