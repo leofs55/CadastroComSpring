f5d8a6411e1b4da300f6e764e47d5d5e4947b2f5
unknown <Leonardo>
2025-04-09 23:01:34 -0300

    feat: Cria Get, Post, Put e DeleteMapping dos Controllores's

--- src/main/java/dev/lest/CadastroNinja/Missoes/MissoesController.java
+++ src/main/java/dev/lest/CadastroNinja/Missoes/MissoesController.java
@@ -1,49 +1,47 @@
 package dev.lest.CadastroNinja.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/missao")
 public class MissoesController {
 
-    @GetMapping("/missao")
+    @GetMapping("/")
     public String missoesHome() {
-        return "Pagina de missoes!";
+        return "<h1> Oi</h1>";
     }
 
     // Adicionar ninja:
 
-    @PostMapping("/missao/criar")
+    @PostMapping("/criar-missao")
     public String criarMissao() {
         return "Missao Criado";
     }
 
     // Procurar ninja por id:
 
-    @GetMapping("/missao/missao-cadastrados")
+    @GetMapping("missoes-cadastradas")
     public String mostrarMissoes() {
         return "Missao Criado";
     }
 
     // Mostrar Ninjas:
 
-    @PostMapping("/missao/missao-por-id")
+    @PostMapping("/missao-por-id")
     public String mostrarMissao() {
         return "Missao Criado";
     }
 
     // Alterar dados de ninja:
 
-    @PutMapping("/missao/alterar")
+    @PutMapping("/alterar-id")
     public String alterarMissao() {
         return "Missao Criado";
     }
 
     // Remover ninja:
 
-    @PostMapping("/missao/deletar-id")
+    @PostMapping("/deletar-id")
     public String deletarMissao() {
         return "Missao Criado";
     }
--- src/main/java/dev/lest/CadastroNinja/Ninjas/NinjaController.java
+++ src/main/java/dev/lest/CadastroNinja/Ninjas/NinjaController.java
@@ -1,49 +1,47 @@
 package dev.lest.CadastroNinja.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/ninja")
 public class NinjaController {
 
-    @GetMapping("/ninja")
+    @GetMapping("/")
     public String index() {
         return "Olá! ";
     }
 
     // Adicionar ninja:
 
-    @PostMapping("/ninja/criar")
+    @PostMapping("/criar-ninja")
     public String criarNinja() {
         return "Ninja Criado";
     }
 
     // Procurar ninja por id:
 
-    @GetMapping("/ninja/ninjas-cadastrados")
+    @GetMapping("/ninjas-cadastrados")
     public String mostrarNinjas() {
         return "Ninja Criado";
     }
 
     // Mostrar Ninjas:
 
-    @PostMapping("/ninja/ninja-por-id")
+    @PostMapping("/ninja-por-id")
     public String mostrarNinja() {
         return "Ninja Criado";
     }
 
     // Alterar dados de ninja:
 
-    @PutMapping("/ninja/alterar")
+    @PutMapping("/alterar")
     public String alterarNinja() {
         return "Ninja Criado";
     }
 
     // Remover ninja:
 
-    @PostMapping("/ninja/deletar-id")
+    @PostMapping("/deletar-id")
     public String deletarNinja() {
         return "Ninja Criado";
     }
