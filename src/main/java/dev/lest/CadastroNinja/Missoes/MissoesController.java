65032de48d0f05d643201629a813ac4352638d3f
unknown <Leonardo>
2025-04-09 21:41:27 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/lest/CadastroNinja/Missoes/MissoesController.java
+++ src/main/java/dev/lest/CadastroNinja/Missoes/MissoesController.java
@@ -1,13 +1,50 @@
 package dev.lest.CadastroNinja.Missoes;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MissoesController {
 
-    @GetMapping("/missoes")
+    @GetMapping("/missao")
     public String missoesHome() {
         return "Pagina de missoes!";
+    }
+
+    // Adicionar ninja:
+
+    @PostMapping("/missao/criar")
+    public String criarMissao() {
+        return "Missao Criado";
+    }
+
+    // Procurar ninja por id:
+
+    @GetMapping("/missao/missao-cadastrados")
+    public String mostrarMissoes() {
+        return "Missao Criado";
+    }
+
+    // Mostrar Ninjas:
+
+    @PostMapping("/missao/missao-por-id")
+    public String mostrarMissao() {
+        return "Missao Criado";
+    }
+
+    // Alterar dados de ninja:
+
+    @PutMapping("/missao/alterar")
+    public String alterarMissao() {
+        return "Missao Criado";
+    }
+
+    // Remover ninja:
+
+    @PostMapping("/missao/deletar-id")
+    public String deletarMissao() {
+        return "Missao Criado";
     }
 }
--- src/main/java/dev/lest/CadastroNinja/Ninjas/NinjaController.java
+++ src/main/java/dev/lest/CadastroNinja/Ninjas/NinjaController.java
@@ -1,13 +1,50 @@
 package dev.lest.CadastroNinja.Ninjas;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NinjaController {
 
-    @GetMapping("/Ninjas")
+    @GetMapping("/ninja")
     public String index() {
         return "Olá! ";
+    }
+
+    // Adicionar ninja:
+
+    @PostMapping("/ninja/criar")
+    public String criarNinja() {
+        return "Ninja Criado";
+    }
+
+    // Procurar ninja por id:
+
+    @GetMapping("/ninja/ninjas-cadastrados")
+    public String mostrarNinjas() {
+        return "Ninja Criado";
+    }
+
+    // Mostrar Ninjas:
+
+    @PostMapping("/ninja/ninja-por-id")
+    public String mostrarNinja() {
+        return "Ninja Criado";
+    }
+
+    // Alterar dados de ninja:
+
+    @PutMapping("/ninja/alterar")
+    public String alterarNinja() {
+        return "Ninja Criado";
+    }
+
+    // Remover ninja:
+
+    @PostMapping("/ninja/deletar-id")
+    public String deletarNinja() {
+        return "Ninja Criado";
     }
 }
