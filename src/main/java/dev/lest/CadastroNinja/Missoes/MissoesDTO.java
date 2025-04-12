1adbf5ea95765336d9c1e38da2545a431b40aad9
unknown <Leonardo>
2025-04-12 16:33:07 -0300

    feat: Cria o corpo da classe de ninja e missoes

--- src/main/java/dev/lest/CadastroNinja/Missoes/MissoesDTO.java
+++ src/main/java/dev/lest/CadastroNinja/Missoes/MissoesDTO.java
@@ -1,4 +1,20 @@
 package dev.lest.CadastroNinja.Missoes;
 
+import dev.lest.CadastroNinja.Ninjas.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesDTO {
+
+    private long id;
+    private String nome;
+    private String dificuldade;
+    private List<NinjaModel> ninjas;
+
 }
