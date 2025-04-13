cd75af8e4190530183527cade7820576f1c78d0f
unknown <Leonardo>
2025-04-13 20:49:07 -0300

    feat: Cria o map do missoesDTO para o MissoesNinjaModel

--- src/main/java/dev/lest/CadastroNinja/Missoes/MissoesMapper.java
+++ src/main/java/dev/lest/CadastroNinja/Missoes/MissoesMapper.java
@@ -1,4 +1,29 @@
 package dev.lest.CadastroNinja.Missoes;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+
+    public MissoesDTO map(MissoesNinjaModel missoesNinjaModel) {
+
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesNinjaModel.getId());
+        missoesDTO.setDificuldade(missoesNinjaModel.getDificuldade());
+        missoesDTO.setNome(missoesNinjaModel.getNome());
+        missoesDTO.setNinjas(missoesNinjaModel.getNinjas());
+
+        return missoesDTO;
+    }
+
+    public MissoesNinjaModel map(MissoesDTO missoesDTO) {
+
+        MissoesNinjaModel missoesNinjaModel = new MissoesNinjaModel();
+        missoesNinjaModel.setId(missoesDTO.getId());
+        missoesNinjaModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesNinjaModel.setNome(missoesDTO.getNome());
+        missoesNinjaModel.setNinjas(missoesDTO.getNinjas());
+
+        return missoesNinjaModel;
+    }
 }
