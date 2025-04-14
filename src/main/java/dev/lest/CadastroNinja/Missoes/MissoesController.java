4f27aea0cfc3ffe95841498a6cda644243bd6063
unknown <Leonardo>
2025-04-13 21:31:23 -0300

    fix: Corrigi o url padrao para as missoes

--- src/main/java/dev/lest/CadastroNinja/Missoes/MissoesController.java
+++ src/main/java/dev/lest/CadastroNinja/Missoes/MissoesController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/missao")
+@RequestMapping("/missoes")
 public class MissoesController {
 
     MissoesService missoesService;
